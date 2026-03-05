2e045e507f8dde7a3e52694ff95dcbb75d47d883
Mubarak Damteh <mubarak.dam@ttbbank.com>
2026-03-06 03:27:45 +0700

    [ADD] Implement employee management views and forms for create, edit, show, and index functionalities

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  resources :employees, controller: "employee"
+  get "showAll", to: "employee#show_all", as: :show_all_employees
+  root "employee#show_all"
 end
